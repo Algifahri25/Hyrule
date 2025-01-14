21223061f95e3556455e293dc72630653a8125e0
Algifahri <algifahri11@gmail.com>
2025-01-14 21:00:39 +0700

    tambah file TOpic2ex4.js

---
+++ Topic2ex4.js
@@ -0,0 +1,81 @@
+//inheritance
+class Laptop {
+    constructor(brand, type, password) {
+        this.brand = brand
+        this.type = type
+        this.encryptedPassword = this.#encrypt(password)
+    }
+
+    #encrypt = (password) => { return `encrypted-version-of-${password}`} 
+    
+    #decrypt = () => {
+    return this.encryptedPassword.split(`encrypted-version-of-`)[1]}
+
+ updatepassword(newPassword) {
+  console.log(`Your old password is : ${this.#decrypt()}`)
+  this.encryptedPassword = this.#encrypt(newPassword)
+  console.log(`Your Password updated to : ${this.#decrypt()}`)
+ }
+
+info() {
+  return `this laptop is ${this.brand} ${this.type}`
+}
+
+run() {
+  return `${this.brand} ${this.type} is on!`
+}
+}
+
+class LaptopGaming extends Laptop {
+  constructor (brand, type, password){
+    super(brand,type,password)
+  }
+
+info() {
+  return `${super.info()} \nAble to Play Games!`
+}
+
+gaming() {
+  return `${super.run()} \nRunning Star Rail and Fate Grand Order`
+}
+}
+
+const gamingCapability = Base => class extends Base{
+  gaming() { 
+    return `Playing Star Rail at 120 fps!`
+  }}
+
+const editingCapability = Base => class extends Base{
+  editing() {
+    return `Let's edit and render at 8k Project`
+  }
+}
+
+class LaptopGaming2 extends gamingCapability(Laptop) {
+  constructor(brand, type, password){
+    super(brand, type, password)
+  }
+
+  run(){
+    return `${super.run()} and ${this.gaming()}`
+  }
+}
+
+class LaptopEditing extends editingCapability(Laptop) {
+  constructor(brand, type, password){
+    super(brand, type, password)
+  }
+  run(){
+    return `${super.run()}, ${this.gaming()}, and ${this.editing()}}`
+  }
+}
+
+let laptop1 = new Laptop ('Acer', 'Normal Laptop', 123)
+let laptop2 = new LaptopGaming ('MSI', 'Hardcore Laptop', 456)
+
+let laptop3 = new LaptopGaming ("Alienware", "Hardcore Laptop", 678)
+laptop3.run()
+laptop3.gaming()
+laptop3.editing()
+console.log()
+console.log()
